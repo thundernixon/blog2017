a8f53947181a6fa322e0fa656726b08b1b362374
Stephen Nixon <stephennixon@StephenxonsMBP2.home>
2017-08-05 12:49:10 -0400

    commit

---
+++ gatsby-config.js
@@ -0,0 +1,31 @@
+module.exports = {
+  siteMetadata: {
+    author: 'You!',
+    title: `Gatsby Default (Blog) Starter`,
+  },
+  plugins: [
+    'gatsby-plugin-catch-links',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        path: `${__dirname}/src/pages`,
+        name: 'pages',
+      },
+    },
+    {
+      resolve: 'gatsby-transformer-remark',
+      options: {
+        plugins: [
+          {
+            resolve: 'gatsby-remark-images',
+            options: {
+              linkImagesToOriginal: false
+            }
+          }
+        ]
+      }
+    },
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-sharp'
+  ],
+}
