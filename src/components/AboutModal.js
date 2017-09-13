6b6c5680debc9b42db605c6fc736bc7c43af2a3a
Stephen Nixon <stephennixon@Stephen-Nixons-MacBook-Pro-2.local>
2017-09-13 15:29:19 +0200

    get about modal working to a medium level

--- src/components/AboutModal.js
+++ src/components/AboutModal.js
@@ -1,4 +1,18 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
+
+const About = styled.div`
+    width: 100vw;
+    height: 100vh;
+    position: fixed;
+    top:0;
+    left:0;
+    background: rgba(56.9%, 8.2%, 100%,.9);
+    font-size: 1.5em;
+    text-align: center;
+    color: white;
+  `;
+
 
 class AboutModal extends Component {
   constructor(props) {
@@ -19,13 +33,19 @@ class AboutModal extends Component {
   render() {
 
     const modal = (
-      <div className="overlay">stuff here</div>
+      <About className="overlay">
+        <p>
+          Hello, my name is Stephen Nixon. 
+        </p> 
+        <button className='button' onClick={this.handleClick}>X
+            {/* {this.state.showModal ? 'ON' : 'OFF'}  */}
+        </button>
+      </About>
     );
     return (
         <div className="about">
           <button className='button' onClick={this.handleClick}>About
-            {this.state.showModal ? 'ON' : 'OFF'} 
-            {/* {this.state.showModal ? modal : ''} */}
+            {/* {this.state.showModal ? 'ON' : 'OFF'}  */}
           </button>
             {this.state.showModal ? modal : ''} 
         </div>
