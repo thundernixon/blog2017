3a553a8eea0e14f270d8147795850f8d58085040
Stephen Nixon <swr410@gmail.com>
2018-09-24 13:44:55 -0400

    update list styles

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Helmet from 'react-helmet';
-import styled, { keyframes } from 'styled-components';
+import styled, {
+  keyframes
+} from 'styled-components';
 import BackIcon from 'react-icons/lib/fa/chevron-left';
 import ForwardIcon from 'react-icons/lib/fa/chevron-right';
 
@@ -12,9 +14,10 @@ import '../css/_vars.scss';
 import '../css/basics.scss';
 import '../css/blog-post.scss';
 import '../css/lists.scss';
+import '../css/tags.scss';
 
 
-const Dropcap = styled.div`
+const Dropcap = styled.div `
   position: absolute;
   font-size: 80vw;
   line-height: 0;
@@ -31,63 +34,102 @@ const Dropcap = styled.div`
 `;
 
 
-export default function Template({ data, pathContext }) {
-  const { markdownRemark: post } = data;
-  const { next, prev } = pathContext;
-  return (
-    <div className="blog-post-container">
-      <Helmet title={`${post.frontmatter.title}`} />
-      <Dropcap
-          className="blog-post-dropcap">
-          {post.excerpt.charAt(0) == "…" ? "" : post.excerpt.charAt(0)}
-        </Dropcap>
-      <div className="blog-post">
-        <h2 className="date">
-          {post.frontmatter.date}
-        </h2>
-        <h1 className="title serif-display">
-          {post.frontmatter.title}
-        </h1>
+export default function Template({
+  data,
+  pathContext
+}) {
+  const {
+    markdownRemark: post
+  } = data;
+  const {
+    next,
+    prev
+  } = pathContext;
+  return ( <
+      div className = "blog-post-container" >
+      <
+      Helmet title = {
+        `${post.frontmatter.title}`
+      }
+      /> <
+      Dropcap className = "blog-post-dropcap" > {
+        post.excerpt.charAt(0) == "…" ? "" : post.excerpt.charAt(0)
+      } <
+      /Dropcap> <
+      div className = "blog-post" >
+      <
+      h2 className = "date" > {
+        post.frontmatter.date
+      } <
+      /h2> <
+      h1 className = "title serif-display" > {
+        post.frontmatter.title
+      } <
+      /h1>
 
-        <div
-          className="blog-post-content"
-          dangerouslySetInnerHTML={{ __html: post.html }}
-        />
-        <Tags className="no-underline" list={post.frontmatter.tags || []} />
-        <div className="navigation">
-          {prev &&
-            <Link className="link prev" to={prev.frontmatter.path}>
-              <BackIcon
-                style={{
-                  height: '0.75rem',
-                  position: 'relative',
-                  top: '0.15rem',
-                }}
-              />
-              <p className="sans-serif">
-                {prev.frontmatter.title}
-              </p>
-            </Link>}
-          {next &&
-            <Link className="link next" to={next.frontmatter.path}>
-              <p className="sans-serif">
-                {next.frontmatter.title}
-              </p>
-              <ForwardIcon
-                style={{
-                  height: '0.75rem',
-                  position: 'relative',
-                  top: '0.15rem',
-                }}
-              />
-            </Link>}
-        </div>
-      </div>
-    </div>
-  );
-}
+      <
+      div className = "blog-post-content"
+      dangerouslySetInnerHTML = {
+        {
+          __html: post.html
+        }
+      }
+      /> <
+      Tags className = "no-underline"
+      list = {
+        post.frontmatter.tags || []
+      }
+      /> <
+      div className = "navigation" > {
+        prev &&
+        <
+        Link className = "link prev"
+        to = {
+          prev.frontmatter.path
+        } >
+        <
+        BackIcon
+        style = {
+          {
+            height: '0.75rem',
+            position: 'relative',
+            top: '0.15rem',
+          }
+        }
+        /> <
+        p className = "sans-serif" > {
+          prev.frontmatter.title
+        } <
+        /p> <
+        /Link>} {
+          next &&
+            <
+            Link className = "link next"
+          to = {
+              next.frontmatter.path
+            } >
+            <
+            p className = "sans-serif" > {
+              next.frontmatter.title
+            } <
+            /p> <
+            ForwardIcon
+          style = {
+            {
+              height: '0.75rem',
+              position: 'relative',
+              top: '0.15rem',
+            }
+          }
+          /> <
+          /Link>} <
+          /div> <
+          /div> <
+          /div>
+        );
+      }
 
-export const pageQuery = graphql`
+      export const pageQuery = graphql `
   query BlogPostByPath($path: String!) {
     markdownRemark(frontmatter: { path: { eq: $path } }) {
       html
