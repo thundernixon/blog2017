cda7000be218c87d52219a2bd445701484d9cb68
Stephen Nixon <swr410@gmail.com>
2018-10-14 08:51:22 -0400

    add default opengraph card

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,6 +3,7 @@ import Helmet from 'react-helmet';
 import styled, { keyframes } from 'styled-components';
 import BackIcon from 'react-icons/lib/fa/chevron-left';
 import ForwardIcon from 'react-icons/lib/fa/chevron-right';
+import DefaultOG from '../assets/typefloundry-og';
 
 import Link from '../components/Link';
 import Tags from '../components/Tags';
@@ -35,40 +36,53 @@ export default function Template({ data, pathContext }) {
   const { next, prev } = pathContext;
   return (
     <div className="blog-post-container">
-      {/* <Helmet title={`${post.frontmatter.title}`} />{' '} */}
-      {/* <Helmet description={`${post.frontmatter.description}`} />{' '} */}
-      {/* <Helmet thumbnail={`${post.frontmatter.thumbnail}`} />{' '} */}
+      {/* <Helmet title={`${post.frontmatter.title}`} /> */}
+      {/* <Helmet description={`${post.frontmatter.description}`} /> */}
+      {/* <Helmet thumbnail={`${post.frontmatter.thumbnail}`} /> */}
       <Helmet>
         <title>`${post.frontmatter.title}`</title>
         <meta name="description" content={post.frontmatter.description} />
         {/* Facebook */}
-        <meta property="og:title" content={post.frontmatter.title}></meta>
-        <meta property="og:description" content={post.frontmatter.description}>
-        <meta property="og:image" content={post.frontmatter.thumbnail}>
-        <meta property="og:url" content={"http://typefloundry.com/" + post.frontmatter.path}>
+        <meta property="og:title" content={post.frontmatter.title} />
+        <meta
+          property="og:description"
+          content={post.frontmatter.description}
+        />
+        {/* if thumbnail, do this */}
+        {/* <meta property="og:image" content={post.frontmatter.thumbnail} /> */}
+        {/* else if no thumbnail, give a default one */}
+        <meta property="og:image" content={DefaultOG} />
+        {/*  */}
+        <meta
+          property="og:url"
+          content={'http://typefloundry.com/' + post.frontmatter.path}
+        />
 
         {/* Twitter */}
-        <meta name="twitter:title" content={post.frontmatter.title}></meta>
-        <meta name="twitter:description" content={post.frontmatter.description}>
-        <meta name="twitter:image" content={post.frontmatter.thumbnail}>
-        <meta name="twitter:card" content="summary_large_image">
+        <meta name="twitter:title" content={post.frontmatter.title} />
+        <meta
+          name="twitter:description"
+          content={post.frontmatter.description}
+        />
+        {/* <meta name="twitter:image" content={post.frontmatter.thumbnail} /> */}
+        <meta name="twitter:image" content={DefaultOG} />
+
+        <meta name="twitter:card" content="summary_large_image" />
       </Helmet>
       <Dropcap className="blog-post-dropcap">
-        {' '}
-        {post.excerpt.charAt(0) == '…' ? '' : post.excerpt.charAt(0)}{' '}
-      </Dropcap>{' '}
+        {post.excerpt.charAt(0) == '…' ? '' : post.excerpt.charAt(0)}
+      </Dropcap>
       <div className="blog-post">
-        <h2 className="date"> {post.frontmatter.date} </h2>{' '}
+        <h2 className="date"> {post.frontmatter.date} </h2>
         <h1 className="title serif-display"> {post.frontmatter.title} </h1>
         <div
           className="blog-post-content"
           dangerouslySetInnerHTML={{
             __html: post.html
           }}
-        />{' '}
-        <Tags className="no-underline" list={post.frontmatter.tags || []} />{' '}
+        />
+        <Tags className="no-underline" list={post.frontmatter.tags || []} />
         <div className="navigation">
-          {' '}
           {prev && (
             <Link className="link prev" to={prev.frontmatter.path}>
               <BackIcon
@@ -77,24 +91,24 @@ export default function Template({ data, pathContext }) {
                   position: 'relative',
                   top: '0.15rem'
                 }}
-              />{' '}
-              <p className="sans-serif"> {prev.frontmatter.title} </p>{' '}
+              />
+              <p className="sans-serif"> {prev.frontmatter.title} </p>
             </Link>
-          )}{' '}
+          )}
           {next && (
             <Link className="link next" to={next.frontmatter.path}>
-              <p className="sans-serif"> {next.frontmatter.title} </p>{' '}
+              <p className="sans-serif"> {next.frontmatter.title} </p>
               <ForwardIcon
                 style={{
                   height: '0.75rem',
                   position: 'relative',
                   top: '0.15rem'
                 }}
-              />{' '}
+              />
             </Link>
-          )}{' '}
-        </div>{' '}
-      </div>{' '}
+          )}
+        </div>
+      </div>
     </div>
   );
 }
