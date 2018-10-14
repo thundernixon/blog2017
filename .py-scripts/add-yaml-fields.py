cff39a068f4c94d0700280d97628398a5abb174b
Stephen Nixon <swr410@gmail.com>
2018-10-14 10:11:18 -0400

    update yaml adder

--- .py-scripts/add-yaml-fields.py
+++ .py-scripts/add-yaml-fields.py
@@ -21,22 +21,8 @@ def filesToEdit(fileName, parentDir):
 def insertYamlAfter(yamlToAdd, yamlEntry, fileName, parentDir):
 
     yamlPattern = yamlEntry + ': "'
-    # print(filesToEdit(fileName, parentDir))
 
     for filePath in filesToEdit(fileName, parentDir):
-        # for line in fileinput.FileInput(filePath,inplace=1):
-        #     print(line)
-        #     if yamlPattern in line:
-        #         print(line)
-        #         newLines = line+yamlToAdd
-        #         line=line.replace(line,newLines)
-        #         print("\t"+newLines)
-
-        # with fileinput.FileInput(fileName, inplace=True, backup='.bak') as file:
-        #     for line in file:
-        #         if yamlPattern in line:
-        #             newLines = line+yamlToAdd
-        #             print(line.replace(line, newLines), end='')
 
         # Read in the file
         with open(filePath, 'r') as file :
@@ -48,7 +34,7 @@ def insertYamlAfter(yamlToAdd, yamlEntry, fileName, parentDir):
             print(line)
             if yamlPattern in line:
                 # Replace the target string
-                filedata = filedata.replace(line, line+yamlToAdd)
+                filedata = filedata.replace(line, line+yamlToAdd, end=' ')
 
         # Write the file out again
         with open(filePath, 'w') as file:
