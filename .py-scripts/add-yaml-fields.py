23ae41e08c5f68cc3c1a0066fa65edb681e73b4a
Stephen Nixon <swr410@gmail.com>
2018-10-14 10:06:08 -0400

    update yaml adder

--- .py-scripts/add-yaml-fields.py
+++ .py-scripts/add-yaml-fields.py
@@ -31,17 +31,30 @@ def insertYamlAfter(yamlToAdd, yamlEntry, fileName, parentDir):
         #         newLines = line+yamlToAdd
         #         line=line.replace(line,newLines)
         #         print("\t"+newLines)
-        with fileinput.FileInput(fileName, inplace=True, backup='.bak') as file:
-            for line in file:
-                if yamlPattern in line:
-                    newLines = line+yamlToAdd
-                    print(line.replace(line, newLines), end='')
 
-            # TODO: make this more robust?
-                # find first "---"
-                # if second "---", stop looking
-                # unless in ``` and ```
-                # don't run if "description" already follows "title"
+        # with fileinput.FileInput(fileName, inplace=True, backup='.bak') as file:
+        #     for line in file:
+        #         if yamlPattern in line:
+        #             newLines = line+yamlToAdd
+        #             print(line.replace(line, newLines), end='')
+
+        # Read in the file
+        with open(filePath, 'r') as file :
+            filedata = file.read()
+
+        if yamlPattern in line:
+            # Replace the target string
+            filedata = filedata.replace(line, line+yamlToAdd)
+
+        # Write the file out again
+        with open(filePath, 'w') as file:
+            file.write(filedata)
+
+        # TODO: make this more robust?
+            # find first "---"
+            # if second "---", stop looking
+            # unless in ``` and ```
+            # don't run if "description" already follows "title"
 
 
 insertYamlAfter(yamlToAdd, "title", "index.md", "src/pages/")
